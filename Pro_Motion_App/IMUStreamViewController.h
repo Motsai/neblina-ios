bce1136f05fae5824607c37f862212f689c76d4e
Amol Deshmukh <amol@mindscrubtech.com>
2015-12-04 20:06:47 +0530

    Added IMUStream Screen.

---
+++ Pro_Motion_App/IMUStreamViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMUStreamViewController.h
+//  Pro_Motion_App
+//
+//  Created by Amol Deshmukh on 04/12/15.
+//  Copyright © 2015 Mindscrub Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMUStreamViewController : UIViewController
+
+@end
---
+++ Pro_Motion_App/IMUStreamViewController.m
@@ -0,0 +1,37 @@
+//
+//  IMUStreamViewController.m
+//  Pro_Motion_App
+//
+//  Created by Amol Deshmukh on 04/12/15.
+//  Copyright © 2015 Mindscrub Technologies. All rights reserved.
+//
+
+#import "IMUStreamViewController.h"
+
+@interface IMUStreamViewController ()
+
+@end
+
+@implementation IMUStreamViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
